284b9bce649c864cebffbd8b8f70f53faf8b71e0
digital <digital@iMac24-5.local>
2022-11-30 11:03:39 +0100

    get random cat

--- network/ViewController.swift
+++ network/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getRandomCat(_ sender: UIButton) {
+        
+        if let url = URL(string: "https://cataas.com/cat/says/hello%20world!"){
+            let urlrequest = URLRequest(url: url)
+            
+            URLSession.shared.dataTask(with: urlrequest) { data, response, err in
+                if let d = data {
+                    self.imageView.image = UIImage(data: d)
+                }
+            }.resume()
+        }
+    }
 }
 
