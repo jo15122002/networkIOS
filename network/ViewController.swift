3c4db5d4bf59af2c9fbc49ccaace186d50b48170
digital <digital@iMac24-5.local>
2022-11-30 10:39:07 +0100

    init

---
+++ network/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  network
+//
+//  Created by digital on 30/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
